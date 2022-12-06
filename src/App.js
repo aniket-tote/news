1e0cee4ad9978a77d16330391a7a1c8712d6d1f4
Aniket Tote <anikettote2001@gmail.com>
2022-12-07 03:22:46 +0530

    added navbar animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,302 @@
-import { Flex } from '@chakra-ui/react'
 import React, { useState } from "react";
 import "./App.css";
-import HomeNav from "./components/HomeNav";
-import Navbar from "./components/Navbar";
 import NewsSection from "./components/NewsSection";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import {
+  Flex,
+  IconButton,
+  useColorMode,
+  Box,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [hamstyle1, togglehamstyle1] = useState("w-full rounded");
+  const [hamstyle2, togglehamstyle2] = useState("w-full rounded");
+  const [hamstyle3, togglehamstyle3] = useState("w-full rounded");
+  const [navstyle, togglenavstyle] = useState(
+    "navbar shadow space-y-4 pt-3 absolute right-full md:relative md:right-0"
+  );
+  const [isClosed, setIsClosed] = useState(false);
+  const [language, toggleLanguage] = useState(false);
+  let isnavopen = false;
+  const toggleNavbar = () => {
+    if (isnavopen) {
+      togglehamstyle1("w-full rounded transition-all duration-500 ease-in-out");
+      togglehamstyle2("w-full rounded transition-all duration-500 ease-in-out");
+      togglehamstyle3("w-full rounded transition-all duration-500 ease-in-out");
+      togglenavstyle(
+        "navbar shadow space-y-4 pt-3 absolute right-full md:relative md:right-0"
+      );
+      isnavopen = false;
+    } else {
+      togglehamstyle1(
+        "w-full rounded rotate-45 translate-y-[6px] transition-all duration-500 ease-in-out"
+      );
+      togglehamstyle2("w-full rounded opacity-0");
+      togglehamstyle3(
+        "w-full rounded -rotate-45 translate-y-[-6px] transition-all duration-500 ease-in-out"
+      );
 
+      togglenavstyle(
+        "navbar shadow space-y-4 pt-3 relative right-0 md:relative md:right-0  transition-all duration-500 ease-in-out"
+      );
+      isnavopen = true;
+    }
+  };
   return (
     <Flex width={["full"]} flexDirection={"column"} className="App">
-      <HomeNav/>
+      <Flex
+        width={"full"}
+        height={"4rem"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        padding={"0rem 1rem"}
+        backgroundColor={colorMode === "dark" ? "gray.800" : "gray.50"}
+      >
+        <Flex
+          justifyContent={"center"}
+          justifySelf={"center"}
+          alignItems={"center"}
+          className="logo space-x-3"
+        >
+          <Flex
+            flexDirection={"column"}
+            justifyContent={"center"}
+            width={"1.5rem"}
+            id="hamburger"
+            className="cursor-pointer space-y-1"
+            display={["inline", "inline", "none"]}
+            onClick={toggleNavbar}
+          >
+            <Box
+              height={"0.15rem"}
+              className={hamstyle1}
+              transform={""}
+              backgroundColor={colorMode === "dark" ? "gray.50" : "gray.800"}
+            ></Box>
+            <Box
+              height={"0.15rem"}
+              className={hamstyle2}
+              backgroundColor={colorMode === "dark" ? "gray.50" : "gray.800"}
+            ></Box>
+            <Box
+              height={"0.15rem"}
+              className={hamstyle3}
+              backgroundColor={colorMode === "dark" ? "gray.50" : "gray.800"}
+            ></Box>
+          </Flex>
+          <Image
+            src="../favicon.ico"
+            alt="logo"
+            className="h-7 w-7"
+            display={["none", "none", "inline"]}
+          />
+          <Text
+            className="logoname text-xl"
+            display={["none", "none", "inline"]}
+          >
+            Inshorts Clone
+          </Text>
+        </Flex>
+
+        <Flex className="space-x-3">
+          <Text
+            className="CategoryHeading text-2xl font-semibold"
+            display={["none", "none", "inline"]}
+          >
+            All News
+          </Text>
+          <Image
+            src="../favicon.ico"
+            alt="logo"
+            className="h-7 w-7"
+            display={["inline", "inline", "none"]}
+          />
+          <Text
+            className="logoname text-xl"
+            display={["inline", "inline", "none"]}
+          >
+            Inshorts Clone
+          </Text>
+        </Flex>
+
+        <Flex className="space-x-2 items-center">
+          <IconButton
+            onClick={toggleColorMode}
+            icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+          />
+        </Flex>
+      </Flex>
       <Flex width={"full"} height={"full"}>
-        <Navbar />
+        <Flex
+          width={"15%"}
+          minWidth={"max-content"}
+          flexDirection={"column"}
+          className={navstyle}
+          backgroundColor={colorMode === "dark" ? "gray.800" : "gray.50"}
+        >
+          <Flex justifyContent={"center"}>
+            <Flex
+              className="languageToggle rounded-md p-1 z-0 w-[8.5rem]"
+              backgroundColor={colorMode === "dark" ? "gray.700" : "gray.200"}
+              onClick={() => toggleLanguage(!language)}
+            >
+              <Box className="english px-2 py-1 rounded cursor-pointer">
+                <Text>English</Text>
+              </Box>
+              <Box className="hindi px-2 py-1 hover:cursor-pointer">
+                <Text>Hindi</Text>
+              </Box>
+              <Box
+                className="background absolute w-16 h-8 rounded z-[-1]"
+                backgroundColor={colorMode === "dark" ? "gray.600" : "gray.300"}
+                marginLeft={language ? "4rem" : "0rem"}
+              ></Box>
+            </Flex>
+          </Flex>
+          <Flex
+            className="heading justify-start px-4 py-2 items-center cursor-pointer space-x-2"
+            onClick={() => setIsClosed(!isClosed)}
+          >
+            <Text className="heading text-2xl font-semibold ">Categories</Text>
+            <Box className="uparrow" display={isClosed ? "none" : "inline"}>
+              <IoIosArrowDown className="w-7 h-7" />
+            </Box>
+            <Box className="downarrow" display={isClosed ? "inline" : "none"}>
+              <IoIosArrowUp className="w-7 h-7" />
+            </Box>
+          </Flex>
+          <Box
+            className="categories flex overflow-hidden"
+            flexDirection={"column"}
+            height={isClosed ? "max-content" : "0px"}
+          >
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category1 rounded mx-2"
+            >
+              <span>All News</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category2 rounded mx-2"
+            >
+              <span>India</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category3 rounded mx-2"
+            >
+              <span>Science</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category4 rounded mx-2"
+            >
+              <span>Technology</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category5 rounded mx-2"
+            >
+              <span>Start Up</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category6 rounded mx-2"
+            >
+              <span>Business</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category7 rounded mx-2"
+            >
+              <span>Sports</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category8 rounded mx-2"
+            >
+              <span>World</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category9 rounded mx-2"
+            >
+              <span>Miscellaneous</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category1 rounded2 mx-2"
+            >
+              <span>Hatke</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category1 rounded2 mx-2"
+            >
+              <span>Automobile</span>
+            </Flex>
+            <Flex
+              justifyContent={"start"}
+              _hover={{
+                backgroundColor: colorMode === "dark" ? "gray.700" : "gray.200",
+              }}
+              padding={"0.5rem 1rem"}
+              className="category1 rounded2 mx-2"
+            >
+              <span>Entertainment</span>
+            </Flex>
+          </Box>
+        </Flex>
         <NewsSection />
       </Flex>
     </Flex>
